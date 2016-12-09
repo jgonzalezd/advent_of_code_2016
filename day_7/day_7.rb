8e9d0f67d45b309f75a5553e64bf441a3fb60a47
Julian Gonzalez <jgonzalezd.sist@gmail.com>
2016-12-09 16:09:14 -0500

    partial commit

---
+++ day_7/day_7.rb
@@ -0,0 +1,27 @@
+require 'byebug'
+# --- Day 7: Internet Protocol Version 7 ---
+#
+# While snooping around the local network of EBHQ, you compile a list of IP addresses (they're IPv7, of course; IPv6 is much too limited). You'd like to figure out which IPs support TLS (transport-layer snooping).
+#
+# An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA. An ABBA is any four-character sequence which consists of a pair of two different characters followed by the reverse of that pair, such as xyyx or abba. However, the IP also must not have an ABBA within any hypernet sequences, which are contained by square brackets.
+#
+# For example:
+#
+# abba[mnop]qrst supports TLS (abba outside square brackets).
+# abcd[bddb]xyyx does not support TLS (bddb is within square brackets, even though xyyx is outside square brackets).
+# aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters must be different).
+# ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a larger string).
+# How many IPs in your puzzle input support TLS?
+
+file = File.open('input.txt')
+file.each_line do |line|
+  outsiders = []
+  insiders = []
+  line.strip.scan(/([^\[\]]+)(\[[a-z]+\])?/).each do |occurrence|
+    outsiders << occurrence[0]
+    insiders << occurrence[1] unless occurrence[1].nil?
+  end
+  insiders = insiders.map { |str| str.gsub(/[\[\]]/,'') }
+  
+end
+file.close
